f2cf4587b9d26e20ba3042c6dcf28281dedf29e9
sallecta <sallecta@inbox.ru>
2019-10-05 16:02:58 +0300

    randome module removed

--- src/mymain.c
+++ src/mymain.c
@@ -1,10 +1,8 @@
 #include "interpreter.c"
-#include "modules/random/init.c"
 #include "modules/math/init.c"
 
 int main(int argc, char *argv[]) {
     interpreter_vm *tp = interpreter_init(argc,argv);
-    random_init(tp);
     math_init(tp);
     interpreter_ez_call(tp,"obfuscatedDataType","interp",interpreter_None);
     interpreter_deinit(tp);
