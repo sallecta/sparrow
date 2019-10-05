424b9ae7cff06e43d516d2729ab5c9a6b25b257f
sallecta <sallecta@inbox.ru>
2019-10-05 17:44:32 +0300

    expanded tests

--- tests/01.py
+++ tests/01.py
@@ -1,7 +1,7 @@
 import sys
 import math
 print("Welcome to ", sys.version)
-print(sys.flags)
-print("hi")
+print("sys.flags:",sys.flags)
+print("5+3/2*4:",5+3/2*4)
 #print("System platform: ", sys.platform)
 #print("os...:",os.path)
