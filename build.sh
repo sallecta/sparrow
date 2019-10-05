5903221b92dff52fe03bc01d02e71d9d2f980270
sallecta <sallecta@inbox.ru>
2019-10-05 17:36:26 +0300

    added build + test onliner option

--- build.sh
+++ build.sh
@@ -5,10 +5,13 @@ source "$PWD/build_scripts/_.functions.sh"
 
 if [ "$1" = "--linux" ] || [ "$1" = "linux" ]; then 
     source "$PWD/build_scripts/linux.sh"
+	if [ "$2" = "test" ]; then 
+		source "$PWD/build_scripts/test.sh"
+	fi	
 elif [ "$1" = "--mingw" ] || [ "$1" = "mingw" ]; then 
     source "$PWD/build_scripts/mingw.sh"
 elif [ "$1" = "--test" ] || [ "$1" = "test" ]; then 
-    $dirbuild/$exename $dirtests/01.py
+    source "$PWD/build_scripts/test.sh"
 elif [ "$1" = "--clean" ] || [ "$1" = "clean" ]; then 
     source "$PWD/build_scripts/clean.sh"
 else
---
+++ build_scripts/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#configs
+##config.sh and functions.sh must be included in caller script
+#end configs
+
+$dirbuild/$exename $dirtests/01.py
